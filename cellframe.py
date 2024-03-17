8e4599c24329f3a0b2ae72058357922c629831d3
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-03-17 11:40:20 +0200

    Add non-validators, save changes

--- cellframe.py
+++ cellframe.py
@@ -33,7 +33,8 @@ async def read_stats(request: Request):
 @app.get("/nodes", response_class=HTMLResponse)
 async def read_stats(request: Request):
     return templates.TemplateResponse("nodes.html", {"request": request, "active_page": "nodes",
-                                                     "node_info": du.fetch_all_node_info()})
+                                                     "node_info": du.fetch_all_node_info(),
+                                                     "nonvalidators": du.fetch_all_non_validator_info()})
 
 @app.get("/richlist", response_class=HTMLResponse)
 async def read_stats(request: Request):
